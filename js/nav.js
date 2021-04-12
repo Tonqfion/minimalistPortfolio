00dbb77664e9a8a46f2e975354c02c81a67504f6
Bertrand Bouland <bbformationcefim@gmail.com>
2021-04-12 18:28:00 +0200

    v7 j'avance

--- js/nav.js
+++ js/nav.js
@@ -5,7 +5,9 @@ toggleMenu.addEventListener('click', function() {
   const open = JSON.parse(toggleMenu.getAttribute('aria-expanded'));
   toggleMenu.setAttribute('aria-expanded', !open);
   menu.hidden = !menu.hidden;
+  toggleMenu.classList.toggle('toggle');
 });
+
 
 
   
